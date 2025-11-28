0e980fc3c22cb06433c800486e201f36fd241551
Vincent Prouillet <github@vincentprouillet.com>
2025-11-28 10:23:11 +0100

    Implement options

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,18 @@ pub use markdown_fence::parse_markdown_fence;
 pub use registry::{HighlightOptions, HighlightedCode, Registry};
 pub use renderers::{Options, html::HtmlRenderer};
 pub use themes::ThemeVariant;
+
+pub const GIALLO_CSS: &str = r#".giallo-l {
+  display: block;
+}
+.giallo-ln {
+  display: inline-block;
+  user-select: none;
+  white-space: pre;
+  margin-right: 0.4em;
+  padding: 0.4em;
+  min-width: 3ch;
+  text-align: right;
+  opacity: 0.8;
+}
+"#;
