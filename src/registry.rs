48b798a89bca25f7715b8e8f436eb411d662c41b
عبد الرحمن صباهي <a.sibahi@gmail.com>
2026-01-02 00:18:54 +0300

    add failure state

--- src/registry.rs
+++ src/registry.rs
@@ -132,17 +132,26 @@ impl Registry {
         }
         let grammar_id = GrammarId(self.grammars.len() as u16);
         let grammar = CompiledGrammar::from_raw_grammar(raw_grammar, grammar_id);
-        let grammar_name = grammar.name.to_lowercase();
         let grammar_scope_name = grammar.scope_name.clone();
-        let file_types = grammar.file_types.clone();
+        let aliases: HashSet<_> = grammar
+            .file_types
+            .iter()
+            .chain(std::iter::once(&grammar.name))
+            .map(|s| s.to_lowercase())
+            .collect();
 
         self.grammars.push(grammar);
         self.grammar_id_by_scope_name
             .insert(grammar_scope_name, grammar_id);
-        self.grammar_id_by_name.insert(grammar_name, grammar_id);
-        for alias in file_types {
-            let alias = alias.to_lowercase();
-            self.grammar_id_by_name.entry(alias).or_insert(grammar_id);
+
+        for alias in aliases {
+            if self
+                .grammar_id_by_name
+                .insert(alias.clone(), grammar_id)
+                .is_some()
+            {
+                return Err(Error::ReplacingGrammarPostLinking(alias.to_owned()));
+            }
         }
         self.injections_by_grammar.push(HashSet::new());
         Ok(())
